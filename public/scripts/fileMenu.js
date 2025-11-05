9bb2c1ed855cd2e957d51cb5d74a2f088e67e3ae
Fa1sall <faisalirfan7c@gmail.com>
2025-11-05 20:29:39 +0530

    feat: implement file info modal logic in fileMenu.js

--- public/scripts/fileMenu.js
+++ public/scripts/fileMenu.js
@@ -10,12 +10,64 @@ function toggleMenu(id) {
   menu.classList.toggle("hidden");
 }
 
-function copyLink(event, link) {
-  navigator.clipboard.writeText(link);
-  const btn = event.target;
-  btn.textContent = "Copied!";
-  setTimeout(() => (btn.textContent = "Copy"), 1500);
+function bytesToMB(bytes) {
+  if (bytes < 1024 * 1024) {
+    return (bytes / 1024).toFixed(2) + " KB";
+  } else {
+    return (bytes / (1024 * 1024)).toFixed(2) + " MB";
+  }
 }
+
+// ---------------- File Info Modal Handling ----------------
+
+async function openFileInfoModal(id) {
+  try {
+    const response = await fetch(`/files/${id}/info`);
+
+    if (!response.ok) {
+      throw new Error(`Failed to get file details: ${response.statusText}`);
+    }
+
+    const fileInfo = await response.json();
+
+    if (!fileInfo || !fileInfo.name || !fileInfo.type) {
+      throw new Error("File details not found");
+    }
+
+    const { name, type, size, createdAt } = fileInfo;
+
+    const fileName = document.getElementById("file-name");
+    fileName.textContent = name;
+
+    const fileType = document.getElementById("file-type");
+    fileType.textContent = type;
+
+    const fileSize = document.getElementById("file-size");
+    fileSize.textContent = bytesToMB(parseInt(size, 10));
+
+    const fileCreated = document.getElementById("file-created");
+    fileCreated.textContent = new Date(createdAt).toLocaleString();
+
+    const fileInfoModal = document.getElementById("file-info-modal");
+    fileInfoModal.classList.remove("hidden");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Closes other unused file menus
+window.addEventListener("click", (e) => {
+  document.querySelectorAll(".file-menu").forEach((menu) => {
+    if (
+      !menu.contains(e.target) &&
+      !menu.previousElementSibling.contains(e.target)
+    ) {
+      menu.classList.add("hidden");
+    }
+  });
+});
+
+// ---------------- Share Modal Handling ----------------
 
 let selectedFileId = null;
 
@@ -25,12 +77,17 @@ function openShareModal(fileId) {
   openModal("share-modal");
 }
 
-function openFileModal(fileId) {}
-
 function closeAllMenus() {
   document
     .querySelectorAll(".file-menu")
     .forEach((m) => m.classList.add("hidden"));
+}
+
+function copyLink(event, link) {
+  navigator.clipboard.writeText(link);
+  const btn = event.target;
+  btn.textContent = "Copied!";
+  setTimeout(() => (btn.textContent = "Copy"), 1500);
 }
 
 async function generateSignedUrl() {
@@ -53,7 +110,7 @@ async function generateSignedUrl() {
     );
 
     if (!response.ok) {
-      throw new Error(`Faield to get signed URL: ${response.statusText}`);
+      throw new Error(`Failed to get signed URL: ${response.statusText}`);
     }
 
     const data = await response.json();
